cfb421a5453de300995523a9c88a02e1579b0e4f
namastegit <akshaykailey1507@gmail.com>
2024-04-29 13:44:43 +0530

    .env file

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -60,10 +60,10 @@ export const NEXT_AUTH = {
           clientId: process.env.GOOGLE_CLIENT_ID || " ",
           clientSecret: process.env. GOOGLE_CLIENT_SECRET || " "
         }),
-        GitHubProvider({
-          clientId: process.env.GITHUB_ID || " ",
-          clientSecret: process.env.GITHUB_SECRET || " "
-        })
+        // GitHubProvider({
+        //   clientId: process.env.GITHUB_ID || " ",
+        //   clientSecret: process.env.GITHUB_SECRET || " "
+        // })
     ],
     secret: process.env.NEXTAUTH_SECRET || "secret",
     callbacks: {   
